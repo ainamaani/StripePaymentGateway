f97d3c4cc38fbd40637e1391b568a6491454bbee
isaacainamaani <aina.isaac2002@gmail.com>
2023-08-07 15:34:14 +0300

    Added the payment code

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const cors = require('cors');
-const stripe = require('stripe');
+const stripe = require('stripe')("sk_test_51NcPAuFo49q8D5COg68LEh5w7uqzPGNp9gPR6gY5hwY8JvbnzkDZ17gIUhEz9U2G1XrJeKSp8N1zmYMAxK1ZoopB001FF8A8DI");
+const { v4:uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+//routes
+app.post('/payment',(req,res)=>{
+    const {product,token} = req.body;
+    const idempotencyKey = uuidv4();
+
+    return stripe.customers.create({
+        email: token.email,
+        source: token.id
+    }).then((customer)=>{
+        stripe.charges.create({
+            amount: product.price * 100,
+            currency: 'usd',
+            customer: customer.id,
+            receipt_email: token.email,
+            description: product.name,
+            shipping: {
+                name: token.card.name,
+                address: {
+                    county: token.card.address_country
+                }
+            }
+        },{idempotencyKey})
+    }).then((result)=>{
+        res.status(200).json(result);
+    }).catch((error)=>{
+        console.log(error);
+    })
+})
 
 app.listen('8282', ()=>{
     console.log('Listening to port 8282');
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import StripeCheckout,{Token} from 'react-stripe-checkout';
+import axios from 'axios';
 
-function App() {
+function App():JSX.Element {
+
+  const [product,setProduct] = useState({
+    name: "Laptop from DELL",
+    price: 1000,
+    productBy: "DELL"
+  });
+  
+
+  const makePayment = (token:Token) =>{
+    const body = {
+      token,
+      product
+    }
+    return axios.post("http://localhost:8282/payment",
+                      JSON.stringify(body),{
+                        headers:{
+                          'Content-Type':'application/json'
+                        }
+                      }
+    ).then((response)=>{
+      const {status} = response;
+      console.log("RESPONSE",response);
+      console.log(status);
+    }).catch((error)=>{
+      console.log(error);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Make a payment</h2>
+      {/* @ts-ignore */}
+      <StripeCheckout stripeKey='pk_test_51NcPAuFo49q8D5COi5DnWZxErp1axl33o1IgfMbmynObwXG0wxrFnPxhzhWtcrp5fXtFX5wA2bN9n3SYQ1dTTvNe00HKNmvShX' 
+      token={makePayment} 
+      name='Buy Laptop'
+      amount={product.price * 100}
+      description="A powerful laptop from DELL" 
+      currency="eur" 
+      billingAddress={true} 
+      shippingAddress={true} 
+      zipCode={true} 
+      >
+          <button>Buy laptop at just ${product.price}</button>
+      </StripeCheckout>
     </div>
   );
 }
